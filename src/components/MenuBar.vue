e40569311c6c84daed2ceb56169ab7175aac87b9
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2020-07-09 17:47:00 -0600

    Got SVG for menu bar sizing correctly

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div id="image">
+            <img src="@/assets/Menu.svg" alt="Kiwi standing on oval">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuBar',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#menubar {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 35vw;
+}
+
+img {
+    width: 100%;
+}
+
+</style>
