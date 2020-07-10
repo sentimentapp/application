af9c25b61c706e62d995743de94d348378f65053
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2020-07-10 10:32:34 -0600

    Fixed a merge conflict

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,6 @@ import Entries from '@/pages/Entries'
 import NewEntry from '@/pages/NewEntry'
 import ViewEntry from '@/pages/ViewEntry'
 import Settings from '@/pages/Settings'
-import Storage from '@/pages/Storage'
 import CapacitorStorage from '@/pages/CapacitorStorage'
 
 Vue.use(Router)
@@ -45,6 +44,7 @@ const router = new Router({
       name: 'ViewEntry',
       component: ViewEntry
     },
+    {
       path: '/capacitor-storage',
       name: 'CapacitorStorage',
       component: CapacitorStorage
