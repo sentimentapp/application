3aa859d3652c97e4c66b972a08adf0e49cd697ec
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2020-07-10 18:44:58 -0600

    Finished a draft for the menu bar

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -4,6 +4,7 @@
     <router-link tag="button" to="/statistics">Statistics</router-link>
     <router-link tag="button" to="/entries">Entries</router-link>
     <router-link to="/settings">Settings</router-link>
+    <menu-bar></menu-bar>
   </div>
 </template>
 
