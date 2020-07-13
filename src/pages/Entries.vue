f1179a122943cfd65c7b15ff0c016d1276f80c69
Logan Gill <2042303@jeffcoschools.us>
2020-07-12 18:41:27 -0600

    Refresh preserves page

--- src/pages/Entries.vue
+++ src/pages/Entries.vue
@@ -3,7 +3,7 @@
     <h1>Journal Entries</h1>
     <router-link tag="button" to="/">Dashboard</router-link>
     <router-link tag="button" to="/entries/new">New Entry</router-link>
-    <div v-for="(entry, index) in $root.entries.slice().reverse()" :key="entry.text">
+    <div v-for="(entry, index) in $root.entries.slice().reverse()" :key="entry.date.toString()">
       <EntryListItem :entry=entry :index=($root.entries.length-1-index) />
     </div>
     <menu-bar></menu-bar>
