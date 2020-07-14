52cef71b1c2801fa8141f4a9ef828f034bc2e579
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2020-07-14 14:10:46 -0600

    Added page transition animations

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-    <router-view />
+    <div id="content">
+      <transition :name="transitionName" mode="out-in">
+        <router-view/>
+      </transition>
+    </div>
+    <menu-bar></menu-bar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return { transitionName: 'fade' }
+  }
 }
 </script>
 
@@ -20,8 +28,30 @@ export default {
   -webkit-font-smoothing: antialiased;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   -moz-osx-font-smoothing: grayscale;
-  overflow:hidden;
 }
 
+/* Sliding
+.slideUp-enter {
+  transform: translateY(300%);
+}
+
+.slideUp-enter-active {
+  transition: transform 1.5s cubic-bezier(0.37, 0, 0.63, 1);
+} */
+
+
+/* Fade */
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 500ms ease;
+}
+
+.fade-leave-active {
+  transition: opacity 500ms ease;
+  opacity: 0;
+}
 
 </style>
--- src/pages/Statistics.vue
+++ src/pages/Statistics.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>Statistics</h1>
     <router-link tag="button" to="/">Dashboard</router-link>
-    <menu-bar></menu-bar>
   </div>
 </template>
 
